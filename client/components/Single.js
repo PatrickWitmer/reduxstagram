c8e0478e050416b8068b050ff7dc7c845cc933bf
WitmerDesigns <patrick@witmerdesigns.com>
2019-11-18 14:02:51 -0700

    single component + photo

--- client/components/Single.js
+++ client/components/Single.js
@@ -1,15 +1,21 @@
 import React from 'react';
+import Photo from './Photo';
+// import comments
 
 const Single = React.createClass({
   render() {
+    // index of the post
+    const i = this.props.posts.findIndex(
+      post => post.code === this.props.params.postId
+    );
+    const post = this.props.posts[i];
+    // get us the post
     return (
       <div className="single-photo">
-        I'm the single view.
+        <Photo i={i} post={post} {...this.props} />;
       </div>
-    )
+    );
   }
-})
+});
 
 export default Single;
-
-
